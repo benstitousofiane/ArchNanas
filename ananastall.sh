46d522d0696c13d32ff5382826294c8734529387
Benstitou Sofiane <129552238+benstitousofiane@users.noreply.github.com>
2023-07-09 18:52:11 +0200

    Update ananastall.sh

--- ananastall.sh
+++ ananastall.sh
@@ -19,10 +19,15 @@ if [ $validation == "o" ]; then
 	echo "NE TOUCHEZ PLUS A RIEN AVANT LE MESSAGE 'FINI LOL'"
 	
 	#------- PARTITIONNEMENT -----------
+
 	#créations de trois partitions :
 	#512M : partition EFI pour le démarrage
 	#TOUT sauf 4G pour la partition racine
 	#4G/Le reste pour la Swap
+
+	#Suppression de tous les partition du disque avec sfdisk
+
+  	sfdisk --delete $disk
 
 	#génère des entré et envoit avec le pipe | les info à fdisk
 	#(ça évite d'utiliser sfdisk plus dure à manipuler)
@@ -51,13 +56,10 @@ if [ $validation == "o" ]; then
 	#Enregistre les modidications
 
 	#Initialisation du Système de fichier :
-	mkfs.fat -F 32 "${disk}1"
-	mkfs.ext4 "${disk}2"
-	mkswap "${disk}3"
+
 
 	#Montage de la patition racine et swapon sur la partition swap:
-	mount "${disk}2" /mnt
-	swapon "${disk}3"
+
 	#-------- FIN PARITIONNEMENT ----------
 
 
