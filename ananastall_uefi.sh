949350726cd55d0280b1322920f175fba41e1a81
Benstitou Sofiane <129552238+benstitousofiane@users.noreply.github.com>
2023-11-19 14:09:29 +0100

    Update ananastall_uefi.sh

--- ananastall_uefi.sh
+++ ananastall_uefi.sh
@@ -160,9 +160,10 @@ if [ $validation == "o" ]; then
 	echo 'greeter-session=lightdm-gtk-greeter' >> /etc/lightdm/lightdm.conf
         \n
 	#Ajout des fonts de meilleurs qualité, affichagede d'autre alphabet (arabe, hébreu, japonais...) et émojis
-	pacman -S noto-fonts, noto-fonts-cjk noto-fonts-emoji
+	pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji
         \n
 	systemctl enable lightdm
+        \n
  	#ajout du son à configurer graphiquement avec pavucontrol si ça ne fonctionne pas via périphérique de sortie et cliquer sur la tout première à coche tout à droite
   	pacman -S pulseaudio pavucontrol --noconfirm
    	\n
